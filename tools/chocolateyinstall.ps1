65c94ba04d6afdb458f63e03bfb3c9b61ea79278
hansnull <t-len@gmx.de>
2017-05-15 12:18:49 +0200

    Initital version 1.90 added.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'fbexport'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.firebirdfaq.org/files/fbexport-1.90.tar.gz'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'FBExport*'
+
+  checksum      = 'c4f768f07225b68bf8cde11021a7b02a'
+  checksumType  = 'md5'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
